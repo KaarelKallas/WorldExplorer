5c04f4d0349062a066442f1b8ebe0ab6ffb3eef0
KaarelKallas <kaarel.kallas@ametikool.ee>
2023-05-29 20:14:06 +0300

    test

--- resources/js/Pages/MapQuiz/PlayQuiz.vue
+++ resources/js/Pages/MapQuiz/PlayQuiz.vue
@@ -3,6 +3,9 @@ import {Head, Link, useForm, router} from '@inertiajs/vue3'
 import {  reactive, ref, watch } from 'vue';
 import answerIcon from "./Icons/icons8-map-pin-48.png"
 import guessIcon from "./Icons/guess.png"
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LMarker, LGeoJson } from "@vue-leaflet/vue-leaflet";
+import L from "leaflet"
 
 const props = defineProps({
     quiz: {
@@ -72,15 +75,20 @@ guess.value.lng = event.latLng.lng()
  const center = reactive({ lat: 58.2449205980223, lng: 22.496933575606914 })
 const currentId = ref(0)
 const form = useForm({
-  lat: null,
-  lng: null,
+  lat: 5,
+  lng: 5,
   id: props.questions[currentQ.value.current].id,
 
 })
 function gMark(event) {
     if (answered.value == 0) {
-        form.lat = event.latLng.lat()
-        form.lng = event.latLng.lng()
+        form.lat = event.x
+        form.lng = event.y
+
+        coords.lat = event.x
+        coords.lng = event.y
+
+        console.log(event)
     }
 
 
@@ -98,21 +106,37 @@ function nextQ() {
     currentQ.value.current++
     answered.value = 0
 }
+let zoom = 5
+const coords = reactive({
+    lat: form.lat,
+    lng: form.lng,
+})
 </script>
 <template class="whitespace-nowrap overflow-hidden w-24 scrollbar-none">
     <Head title="Map Quiz"/>
     <GuestLayout class="scrollbar-none">
-    <GMapMap id="vue-map" ref="myMapRef" :center="center" :zoom="10" map-type-id="hybrid" class="h-screen w-screen"  @click="gMark">
-        <GMapMarker :icon="answerIcon" :animation="1"   :position="{lat:location.lat, lng:location.lng}" v-if="answered == 1">
+        <div class="w-screen h-screen absolute -z-10">
+    <l-map :use-global-leaflet="true" ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]"  @click="gMark">
+        <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+      <l-marker :lat-lng="[coords.lat, coords.lng]" draggable> </l-marker>
+    </l-map>
+
+</div>
+        <!-- <GMapMarker :icon="answerIcon" :animation="1"   :position="{lat:location.lat, lng:location.lng}" v-if="answered == 1">
           </GMapMarker>
           <GMapPolyline v-if="answered == 1"
       :path="[{lat:form.lat, lng:form.lng}, {lat:location.lat, lng:location.lng}]"
         ref="polyline" />
        <GMapMarker :icon="guessIcon"  :position="{lat:form.lat, lng:form.lng}">
+    </GMapMarker> -->
            <div class="absolute right-24 top-0">
                <img class="rounded-b-3xl w-auto h-40 " :src="questions[currentQ.current].image" alt="">
            </div>
-        <div class="absolute top-0 inset-x-96 flex justify-between filter backdrop-blur-md bg-gray-300/30 p-5 items-center align-center rounded-b-3xl">
+        <div class="absolute z-10 top-0 inset-x-96 flex justify-between filter backdrop-blur-md bg-gray-300/30 p-5 items-center align-center rounded-b-3xl">
 
 
     <h1 class="font-raleway box-border text-4xl text-white">{{ questions[currentQ.current].question }}</h1>
@@ -131,7 +155,7 @@ function nextQ() {
             <button v-if="currentQ.current == questions.length - 1 && answered == 1" @click="router.get('/quizmap/show/' + questions[0].map_quiz_id)" class="w-32 h-24  text-2xl text-white absolute flex bottom-5 justify-center inset-x-[54rem]  backdrop-blur-3xl bg-white/30  hover:bg-lime-500/30 rounded-3xl pt-4">PLAY AGAIN</button>
             <button v-if="answered == 1 && currentQ.current != questions.length - 1" @click="nextQ" class="w-32 h-16  text-2xl text-white absolute flex bottom-5 justify-center inset-x-[54rem]  backdrop-blur-3xl bg-white/30  hover:bg-lime-500/30 rounded-3xl pt-4">NEXT</button>
             <form @submit.prevent="submit"><button type="submit" v-if="answered == 0 && currentQ.current != questions.length" :disabled="form.processing" class="w-32 h-16  text-2xl text-white absolute flex bottom-5 justify-center inset-x-[54rem] pt-4 backdrop-blur-3xl bg-white/30  hover:bg-lime-500/30 rounded-3xl">ANSWER</button></form>
-          </GMapMarker>
+
           <div v-if="currentQ.current == questions.length - 1 && answered == 1" class="absolute inset-1/4 flex justify-between filter backdrop-blur-md bg-gray-300/30 p-1 items-center align-center rounded-3xl">
               <h1 class="font-raleway box-border text-4xl text-white rounded-lg font-bold pl-24">Game over!</h1>
               <h1 class="font-raleway box-border text-4xl text-lime-200 rounded-lg font-bold pl-24">Your score is: {{ Math.round(score) }}</h1>
@@ -141,6 +165,7 @@ function nextQ() {
                   </svg>
               </Link> <br>
            </div>
-    </GMapMap>
+
+
 </GuestLayout>
   </template>
